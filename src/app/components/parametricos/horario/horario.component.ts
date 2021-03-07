8a6f25c1b67f23fd63f93cef279cfa49c32fea3f
LorenaAcosta <lorena.acosta95@gmail.com>
2021-03-07 14:37:25 -0300

    commit 7/3/2021

--- src/app/components/parametricos/horario/horario.component.ts
+++ src/app/components/parametricos/horario/horario.component.ts
@@ -6,6 +6,7 @@ import { Observable, throwError } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { HorarioService } from 'src/app/services/servicios/horario.service';
+import { UtilesService } from 'src/app/services/servicios/utiles.service';
 
 @Component({
   selector: 'app-horario',
@@ -19,7 +20,7 @@ export class HorarioComponent implements OnInit {
     horarioId: [''],
     horaFin: ['', Validators.required],
     horaInicio: ['', Validators.required],
-    empleadoId: ['', Validators.required],
+    empleadoId: ['']
   });
   categorias: any[] = [];
   empleados: any[] = [];
@@ -27,6 +28,7 @@ export class HorarioComponent implements OnInit {
 
   constructor(private fb:             FormBuilder,
               private horarioService: HorarioService,
+              private util:           UtilesService,
               private route:          ActivatedRoute,
               private router:         Router) {
 
@@ -34,14 +36,14 @@ export class HorarioComponent implements OnInit {
       horarioId: [''],
       horaFin: ['', Validators.required],
       horaInicio: ['', Validators.required],
-      empleadoId: ['', Validators.required],
+      empleadoId: [''],
       });
 }
 
 
   ngOnInit() {
      const id = this.route.snapshot.params.id;
-     console.log(id);
+     //console.log(id);
      if (typeof id !== 'undefined') {
       this.form2 = this.fb.group({
         horarioId: [''],
@@ -53,39 +55,37 @@ export class HorarioComponent implements OnInit {
        this.horarioService.obtenerHorario(id)
        .subscribe ((data: any) => {
         this.form2.controls.empleadoId.setValue(data.empleadoId.nombre.concat(" ").concat(data.empleadoId.apellido) );
-        this.form2.controls.horaFin.setValue(data.horaFin);
-        this.form2.controls.horaInicio.setValue(data.horaInicio); 
+        this.form2.controls.horaFin.setValue( data.horaFin );
+        this.form2.controls.horaInicio.setValue(data.horaInicio ); 
     });
   }
 }
 
   ver() {
-    console.warn(this.form2.value);
+   // console.warn(this.form2.value);
   }
 
   guardar() {
     const id = this.route.snapshot.params.id;
     let peticion: Observable<any>;
-
     peticion = this.horarioService.obtenerHorario(id);
-    peticion.subscribe((result: any) =>  {
-      this.horarioId = result.horarioId.value;
+    peticion.subscribe((r: any) =>  {
+      //console.log(r);
+      this.form2.controls.empleadoId.setValue(r.empleadoId.empleadoId);
+      this.form2.controls.horarioId.setValue(r.horarioId);
+      console.log(this.form2.value);
+      peticion = this.horarioService.modificarRecurso(this.form2.value, this.form2.controls.
+        horarioId.value);
+      peticion.subscribe((result: any) =>  {
+        Swal.fire(
+          'Guardado!',
+          'Se actualizaron los datos!',
+          'success'
+        );
+        this.router.navigate(['/empleado/listar']);
+      });
+
     });
-   
-    this.form2.controls.horarioId.setValue( this.horarioId );
-    this.form2.controls.empleadoId.setValue( id );
-    console.log(this.form2.value);
-   
-    peticion = this.horarioService.modificarRecurso(this.form2.value, id);
-    peticion.subscribe((result: any) =>  {
-      Swal.fire(
-        'Guardado!',
-        'Se actualizaron los datos!',
-        'success'
-      );
-      this.router.navigate(['/empleado/listar']);
-    });
-   
   }
 
 
