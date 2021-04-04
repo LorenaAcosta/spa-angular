b3b0b50daafdf1b7b6b5d3d15c28e54aa48c3535
elias.ocampo <eljo1391@gmail.com>
2021-04-04 19:20:10 -0400

    correcion venta service

--- src/app/services/servicios/venta.service.ts
+++ src/app/services/servicios/venta.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
