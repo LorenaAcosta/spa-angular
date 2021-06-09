caf0acc4dbc4863d0bd6921d4ad6f68fe2a93970
elias.ocampo <eljo1391@gmail.com>
2021-06-09 09:02:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/spa-2021-angular'));
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
     //res.sendFile(path.join(__dirname + '/dist/spa-2021-angular/index.html'));
     res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
