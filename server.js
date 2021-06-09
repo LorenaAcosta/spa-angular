ac74721395b9ae9438d037458d8a3e1502b4015d
elias.ocampo <eljo1391@gmail.com>
2021-06-09 08:50:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/spa-2021-angular'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/spa-2021-angular/index.html'));
+    //res.sendFile(path.join(__dirname + '/dist/spa-2021-angular/index.html'));
+    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
